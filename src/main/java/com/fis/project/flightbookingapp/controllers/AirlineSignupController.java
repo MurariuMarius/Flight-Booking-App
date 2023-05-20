df83343a9b3209a56b9bc1237bf5ba1ddcd5bc8f
Marius Murariu <mam.murariu@gmail.com>
2023-05-20 17:15:44 +0300

    FBA-1 Added GUI for role-based signup

---
+++ src/main/java/com/fis/project/flightbookingapp/controllers/AirlineSignupController.java
@@ -0,0 +1,26 @@
+package com.fis.project.flightbookingapp.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AirlineSignupController {
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField countryField;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private TextField phoneNumberField;
+
+
+    @FXML
+    void createAirlineAccount() {
+
+    }
+}
---
+++ src/main/java/com/fis/project/flightbookingapp/controllers/CustomerSignUpController.java
@@ -0,0 +1,25 @@
+package com.fis.project.flightbookingapp.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CustomerSignUpController {
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField countryField;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    void createCustomerAccount() {
+
+    }
+}
