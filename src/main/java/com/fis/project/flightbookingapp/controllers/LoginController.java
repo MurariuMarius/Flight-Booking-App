056d2a838f8d83ceadfe0ef2acecd29e03f19d17
Marius Murariu <mam.murariu@gmail.com>
2023-05-20 13:36:57 +0300

    FBA-1 Added login page and controller

---
+++ src/main/java/com/fis/project/flightbookingapp/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package com.fis.project.flightbookingapp.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Button signupButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
