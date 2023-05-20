e586c947381084bcd4969b3464c056a25a17ebc8
Marius Murariu <mam.murariu@gmail.com>
2023-05-20 17:14:13 +0300

    FBA-1 Added sign-up behaviour + minor fixes

--- src/main/java/com/fis/project/flightbookingapp/controllers/LoginController.java
+++ src/main/java/com/fis/project/flightbookingapp/controllers/LoginController.java
@@ -1,9 +1,14 @@
 package com.fis.project.flightbookingapp.controllers;
 
+import com.fis.project.flightbookingapp.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -28,8 +33,21 @@ public class LoginController {
     private TextField usernameField;
 
     @FXML
-    void initialize() {
-
+    void goToSignupPage() {
+        try {
+            Stage stage = (Stage) signupButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("signup.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            stage.setTitle("Flight Booking App - Log In");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    @FXML
+    void login() {
+        // TODO
+    }
 }
