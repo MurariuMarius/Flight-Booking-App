ca416d602f42d3e5c6230a23971bb2e239e77d96
Marius Murariu <mam.murariu@gmail.com>
2023-05-21 20:57:01 +0300

    FBA-12 Added Nitrite Java dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,12 @@ module com.fis.project.flightbookingapp {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires nitrite;
 
     opens com.fis.project.flightbookingapp to javafx.fxml;
     exports com.fis.project.flightbookingapp;
     exports com.fis.project.flightbookingapp.controllers;
     opens com.fis.project.flightbookingapp.controllers to javafx.fxml;
+    opens com.fis.project.flightbookingapp.services;
+    opens com.fis.project.flightbookingapp.model;
 }
