9552df228c65e78ba77e9e6e464d19daefc1d784
Marius Murariu <mam.murariu@gmail.com>
2023-05-22 07:25:27 +0300

    FBA-12 Added credit card database support

---
+++ src/main/java/com/fis/project/flightbookingapp/model/Client.java
@@ -0,0 +1,42 @@
+package com.fis.project.flightbookingapp.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Client extends User {
+    private List<CreditCard> creditCards;
+
+    public Client () {}
+    public Client(String username, String password, String phoneNumber, String address, String country, String email) {
+        super(username, password, phoneNumber, address, country, email);
+    }
+
+    public List<CreditCard> getCreditCards() {
+        return creditCards;
+    }
+
+    public void setCreditCards(List<CreditCard> creditCards) {
+        this.creditCards = creditCards;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Client client = (Client) o;
+        return Objects.equals(creditCards, client.creditCards);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), creditCards);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "creditCards=" + creditCards +
+                "} " + super.toString();
+    }
+}
