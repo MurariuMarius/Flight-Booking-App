b4b541ccd8f45d331091847046366556a848c7c2
Marius Murariu <mam.murariu@gmail.com>
2023-05-22 14:17:38 +0300

    FBA-1 Insert new user into database

--- src/main/java/com/fis/project/flightbookingapp/controllers/AirlineSignupController.java
+++ src/main/java/com/fis/project/flightbookingapp/controllers/AirlineSignupController.java
@@ -1,5 +1,10 @@
 package com.fis.project.flightbookingapp.controllers;
 
+import com.fis.project.flightbookingapp.exceptions.UserAlreadyExistsException;
+import com.fis.project.flightbookingapp.model.Airline;
+import com.fis.project.flightbookingapp.services.AirlineUserService;
+import com.fis.project.flightbookingapp.services.EmailValidationService;
+import com.fis.project.flightbookingapp.services.PhoneValidationService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -7,7 +12,7 @@ import javafx.scene.control.TextField;
 public class AirlineSignupController {
 
     @FXML
-    private TextField iataCodeField;
+    private TextField icaoCodeField;
 
     @FXML
     private TextField addressField;
@@ -22,15 +27,46 @@ public class AirlineSignupController {
     private TextField phoneNumberField;
 
     @FXML
+    private TextField emailField;
+
+    @FXML
     private TextField airOperatorCertificateField;
+
+    private Airline airline;
 
     @FXML
     void createAirlineAccount() {
-        if (iataCodeField.getText().isBlank() || addressField.getText().isBlank() || countryField.getText().isBlank() ||
+        if (icaoCodeField.getText().isBlank() || addressField.getText().isBlank() || countryField.getText().isBlank() ||
         phoneNumberField.getText().isBlank() || airOperatorCertificateField.getText().isBlank()) {
-            // TODO Alert user for missing fields
+            System.out.println("Missing fields");
             return;
         }
-        // TODO Insert new user in db
+        if (!EmailValidationService.isValid(emailField.getText())) {
+            System.out.println("Invalid email");
+            return;
+        }
+
+        if (!PhoneValidationService.isValid(phoneNumberField.getText().trim())) {
+            System.out.println("Invalid phone number");
+            return;
+        }
+
+        airline.setCountry(countryField.getText().trim());
+        airline.setAddress(addressField.getText().trim());
+        airline.setEmail(emailField.getText().trim());
+        airline.setPhoneNumber(phoneNumberField.getText().trim());
+        airline.setIcaoCode(icaoCodeField.getText());
+        airline.setAirOperatorCertificate(airOperatorCertificateField.getText());
+        
+        try {
+            AirlineUserService.addUser(airline);
+            System.out.println("Account successfully created");
+        } catch (UserAlreadyExistsException e) {
+            System.out.println(e);
+        }
+    }
+
+    void initData(Airline airline) {
+        this.airline = airline;
     }
 }
--- src/main/java/com/fis/project/flightbookingapp/controllers/SignupController.java
+++ src/main/java/com/fis/project/flightbookingapp/controllers/SignupController.java
@@ -2,9 +2,13 @@ package com.fis.project.flightbookingapp.controllers;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import com.fis.project.flightbookingapp.Main;
+import com.fis.project.flightbookingapp.model.Airline;
+import com.fis.project.flightbookingapp.model.Client;
+import com.fis.project.flightbookingapp.model.User;
+import com.fis.project.flightbookingapp.services.PasswordEncodingService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -47,21 +51,31 @@ public class SignupController {
         String option = userRole.getSelectionModel().getSelectedItem();
         if (option == null || usernameField.getText().isBlank() || passwordField.getText().isBlank() || repeatedPasswordField.getText().isBlank()) {
             // TODO Alert user to fill out all fields
+            System.out.println("Alert user to fill out all fields");
             return;
         }
         if (!passwordField.getText().equals(repeatedPasswordField.getText())) {
             // TODO Alert user that passwords don't match
+            System.out.println("user that passwords don't match");
             return;
         }
 
         // TODO Check username not already existent
+        String username = usernameField.getText().trim().toLowerCase();
+        if (username.contains(" ") || passwordField.getText().contains(" ")) {
+            // TODO Invalid input
+        }
 
         FXMLLoader fxmlLoader;
+        User user;
+
+        // Pass new User to appropriate controller
         if (userRole.getSelectionModel().getSelectedItem().equals("Customer")) {
             fxmlLoader = new FXMLLoader(Main.class.getResource("customer-signup-form.fxml"));
         } else {
             fxmlLoader = new FXMLLoader(Main.class.getResource("airline-signup-form.fxml"));
         }
+
         try {
             Stage stage = (Stage) signupButton.getScene().getWindow();
             Scene scene = new Scene(fxmlLoader.load(), 600, 400);
@@ -71,6 +85,19 @@ public class SignupController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        if (userRole.getSelectionModel().getSelectedItem().equals("Customer")) {
+            user = new Client();
+            CustomerSignUpController controller = fxmlLoader.getController();
+            controller.initData((Client) user);
+        } else {
+            user = new Airline();
+            AirlineSignupController controller = fxmlLoader.getController();
+            controller.initData((Airline) user);
+        }
+
+        user.setUsername(username);
+        user.setPassword(PasswordEncodingService.encodePassword(username,passwordField.getText()));
     }
 
 }
