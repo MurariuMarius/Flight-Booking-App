af7b1e5968cd8c11d9af461ea305b09889da9d66
Marius Murariu <mam.murariu@gmail.com>
2023-05-22 14:38:24 +0300

    FBA-1 Added checkCredentials method to user database services

--- src/main/java/com/fis/project/flightbookingapp/services/AirlineUserService.java
+++ src/main/java/com/fis/project/flightbookingapp/services/AirlineUserService.java
@@ -1,9 +1,11 @@
 package com.fis.project.flightbookingapp.services;
 
+import com.fis.project.flightbookingapp.exceptions.InvalidCredentialsException;
 import com.fis.project.flightbookingapp.exceptions.UserAlreadyExistsException;
 import com.fis.project.flightbookingapp.model.Airline;
-import com.fis.project.flightbookingapp.model.User;
+import org.dizitart.no2.objects.Cursor;
 import org.dizitart.no2.objects.ObjectRepository;
+import org.dizitart.no2.objects.filters.ObjectFilters;
 
 import java.util.List;
 import java.util.Objects;
@@ -21,6 +23,16 @@ public class AirlineUserService {
             if (Objects.equals(user.getUsername(), u.getUsername())) {
                 throw new UserAlreadyExistsException(user.getUsername());
             }
+        }
+    }
+
+    public static void checkCredentials(String username, String password) throws InvalidCredentialsException {
+        Cursor<Airline> cursor = airlineUserRepository.find(ObjectFilters.and(
+                ObjectFilters.eq("username", username),
+                ObjectFilters.eq("password", PasswordEncodingService.encodePassword(username, password)))
+        );
+        if (cursor.totalCount() != 1) {
+            throw new InvalidCredentialsException();
         }
     }
 
--- src/main/java/com/fis/project/flightbookingapp/services/ClientUserService.java
+++ src/main/java/com/fis/project/flightbookingapp/services/ClientUserService.java
@@ -1,8 +1,11 @@
 package com.fis.project.flightbookingapp.services;
 
+import com.fis.project.flightbookingapp.exceptions.InvalidCredentialsException;
 import com.fis.project.flightbookingapp.exceptions.UserAlreadyExistsException;
 import com.fis.project.flightbookingapp.model.Client;
+import org.dizitart.no2.objects.Cursor;
 import org.dizitart.no2.objects.ObjectRepository;
+import org.dizitart.no2.objects.filters.ObjectFilters;
 
 import java.util.List;
 import java.util.Objects;
@@ -20,6 +23,16 @@ public class ClientUserService {
             if (Objects.equals(user.getUsername(), u.getUsername())) {
                 throw new UserAlreadyExistsException(user.getUsername());
             }
+        }
+    }
+
+    public static void checkCredentials(String username, String password) throws InvalidCredentialsException {
+        Cursor<Client> cursor = clientUserRepository.find(ObjectFilters.and(
+                ObjectFilters.eq("username", username),
+                ObjectFilters.eq("password", PasswordEncodingService.encodePassword(username, password)))
+        );
+        if (cursor.totalCount() != 1) {
+            throw new InvalidCredentialsException();
         }
     }
 
