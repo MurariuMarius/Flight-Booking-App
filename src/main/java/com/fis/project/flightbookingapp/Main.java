a385c80fd5cf9464575d7d72bfef21e1e82dfedb
adonisopris <adonis.opris@student.upt.ro>
2023-05-30 17:44:41 +0300

    FBA-44 updated the design

--- src/main/java/com/fis/project/flightbookingapp/Main.java
+++ src/main/java/com/fis/project/flightbookingapp/Main.java
@@ -18,14 +18,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(),1000,700);
         stage.setTitle("Flight Booking App");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) throws UserAlreadyExistsException, FileNotFoundException, AirportAlreadyExistsException {
-        /*AirportService.addAirports(AirportGetterService.getAirportsFromJSON());*/
+        AirportService.addAirports(AirportGetterService.getAirportsFromJSON());
         launch();
     }
 }
